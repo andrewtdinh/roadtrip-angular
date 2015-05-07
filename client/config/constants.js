262e37ead7c071b7d478b4d9321ef51560054678
Andrew Dinh <andrewdinh74@yahoo.com>
2015-05-06 17:15:32 -0700

    change package.json version

--- client/config/constants.js
+++ client/config/constants.js
@@ -2,4 +2,4 @@
 
 angular.module('roadtrip')
 .constant('firebaseUrl', 'https://myfb.firebaseio.com/')
-.constant('nodeUrl', 'http://localhost:8000');
+.constant('nodeUrl', 'https://young-brushlands-7557.herokuapp.com/');
