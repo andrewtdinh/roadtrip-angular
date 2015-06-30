20230f0184cc413b92161a754863915a0c86b143
Andrew Dinh <andrewdinh74@yahoo.com>
2015-06-29 22:03:46 -0700

    Change nodeUrl before pushing to live

--- client/config/constants.js
+++ client/config/constants.js
@@ -2,5 +2,5 @@
 
 angular.module('roadtrip')
 .constant('firebaseUrl', 'https://myfb.firebaseio.com/')
-.constant('nodeUrl', 'http://localhost:8000');
-// .constant('nodeUrl', 'https://young-brushlands-7557.herokuapp.com');
+.constant('nodeUrl', 'https://young-brushlands-7557.herokuapp.com');
+// .constant('nodeUrl', 'http://localhost:8000');
